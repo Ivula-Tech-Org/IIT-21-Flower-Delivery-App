d06f92a8a50ca5db87d23589079b99349c3086aa
clifford-obure <cliffobure@gmail.com>
2023-05-10 11:12:22 -0700

    front-end

--- services/front_end_services.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-
-
-const front_end_service = express.Router();
-
-export default front_end_service.get('/', async(req,res)=>{
-    res.send('This is the front end services');
-})
